cb610f5c4f2661ca3cdab1a65b40eb53b9e2de66
ErmekOrazbekov <154459511+surf100@users.noreply.github.com>
2024-01-26 19:36:42 +0600

    Second init

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
+        //create ArrayList to store objects of type person
         ArrayList<Person> person_data = new ArrayList<>();
+        //creating objects of Employee and Student classes
         Employee Rasul = new Employee("Rasul","Satmyrza","Trader",300000.00);
         Employee Abylai = new Employee("Abylai","Ahmet","Activist",400000.00);
         Student Davlat = new Student("Davlat","Sunnatov",3.46);
         Student Beka = new Student("Bekzat","Nakiev",2.0);
+
         person_data.add(Rasul);
         person_data.add(Abylai);
         person_data.add(Davlat);
         person_data.add(Beka);
-        Collections.sort(person_data, new Comparator<Person>() {
-            @Override
-            public int compare(Person person1, Person person2) {
-                return Double.valueOf(person1.getPaymentAmount()).compareTo(person2.getPaymentAmount());
-            }
-        });
-        for(Person person : person_data){
-            System.out.println(person);
-        }
+        //sorting them with compareTo method,which in Person class
+        Collections.sort(person_data);
 
+        printData(person_data);
+        }
+        //creating printData method
+        public static void printData(Iterable<Person> persons){
+            for (Person person : persons){
+                //iterating through ArrayList and printing its objects
+                System.out.println(person);
+            }
         }
     }
