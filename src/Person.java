a802d489f8d1499ea46948f4c6b9ccacd081c361
ErmekOrazbekov <154459511+surf100@users.noreply.github.com>
2024-01-26 19:38:21 +0600

    Second init

--- src/Person.java
+++ src/Person.java
@@ -1,20 +1,27 @@
-public abstract class Person implements Payable,Comparable<Person>{
-    private static int id_gen = 0;
+import java.util.ArrayList;
+
+public abstract class Person implements Payable, Comparable<Person> {
+    private static int id_gen = 0; 
     private int id;
     private String name;
     private String surname;
-    public Person(){
-    }
-    public Person(String name,String surname){
+
+    // Default constructor
+    public Person() {}
+
+    // parameterized constructor to initialize name and surname
+    public Person(String name, String surname) {
         setName(name);
         setSurname(surname);
-        this.id_gen++;
-        this.id = id_gen;
-
+        this.id_gen++; 
+        this.id = id_gen; 
     }
-    public int compareTo(Person person){
+
+    // implementation of Comparable interface to compare persons
+    public int compareTo(Person person) {
         return Double.compare(this.getPaymentAmount(), person.getPaymentAmount());
     }
+
 
     public int getId() {
         return id;
@@ -35,10 +42,13 @@ public abstract class Person implements Payable,Comparable<Person>{
     public String getSurname() {
         return surname;
     }
+
+    // abstract method to be implemented by subclasses
     public abstract String getPosition();
+
     @Override
     public String toString() {
-        return  getPosition() +
+        return getPosition() +
                 "\nid = " + getId() +
                 "\nname = " + name +
                 "\nsurname = " + surname;
